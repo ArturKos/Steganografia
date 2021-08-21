8e4d8a8177f5b439bbb3b183879e202bc61e6deb
Artur Kos <R_E_D_O_X@wp.pl>
2021-08-21 22:31:15 +0200

    Program ukrywa tekst w obrazie oraz zabezpiecza hasłem. Napisane w C#.

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace kodowanie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(394, 288);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(472, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(394, 288);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "wczytaj obraz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(232, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "wstaw informację do obrazu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(472, 328);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "odczytaj ukrytą informację";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(717, 328);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "zapisz zmoodyfikowany plik";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 404);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(394, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Artur Kos";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(472, 404);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(394, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 373);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Tekst do zaszyfrowania";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(469, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Tekst odszyfrowany";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 447);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Hasło";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 465);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(394, 20);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "Tajne hasło";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 497);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using CryptAES;
+using System.Security.Cryptography;
+namespace kodowanie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static void Set(ref byte aByte, int pos, bool value)
+        {
+            if (value)
+            {
+                //left-shift 1, then bitwise OR
+                aByte = (byte)(aByte | (1 << pos));
+            }
+            else
+            {
+                //left-shift 1, then take complement, then bitwise AND
+                aByte = (byte)(aByte & ~(1 << pos));
+            }
+        }
+
+        public static bool Get(byte aByte, int pos)
+        {
+            //left-shift 1, then bitwise AND, then check for non-zero
+            return ((aByte & (1 << pos)) != 0);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+        // kod haminga
+        // rozproszenie informacji, permutacja za pomocą random
+        //http://82.145.73.240/index.php/Informatyka_N2
+        private void button2_Click(object sender, EventArgs e)
+        {
+            char dlugosc_ciagu = (char)textBox1.Text.Length;
+            //char[] text = string.Concat(textBox1.Text.Select(aat => Convert.ToString(aat, 2).PadLeft(8))).ToArray();
+            byte[] bajty= Encoding.UTF8.GetBytes(textBox1.Text);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(textBox3.Text);
+            passwordBytes = SHA512.Create().ComputeHash(passwordBytes);
+            byte[] bytesEncrypted = AES.AES_Encrypt(bajty, passwordBytes);
+            bajty = bytesEncrypted;
+            dlugosc_ciagu = (char)bajty.Length;
+            BitArray text = new BitArray(bajty);
+            Bitmap b = new Bitmap(pictureBox1.Image);
+            int x = b.Width;
+            int y = b.Height;
+            Color color;
+            Color ncolor;
+            int bit = 7;
+            byte br, bg = 0, bb = 0;
+            bool gr;
+            byte len = 0;
+            for (int i = 0; i < x; i++)
+                for (int j = 0; j < y; j++)
+                {
+                    color = b.GetPixel(i, j);
+                    br = color.R;
+                    bg = color.G;
+                    bb = color.B;
+
+                    if (bit >= 0)
+                    {
+                        //zmiana bitów
+                        gr = Get((byte)dlugosc_ciagu, bit);
+                        Set(ref br, 0, gr);
+
+                        if (bit >= 1)
+                        {
+                            bg = color.G;
+                            gr = Get((byte)dlugosc_ciagu, --bit);
+                            Set(ref bg, 0, gr);
+                        }
+                        if (bit >= 1)
+                        {
+                            bb = color.B;
+                            gr = Get((byte)dlugosc_ciagu, --bit);
+                            Set(ref bb, 0, gr);
+                        }
+
+                        //wpisanie kloroów
+                        ncolor = Color.FromArgb(br, bg, bb);
+                        b.SetPixel(i, j, ncolor);
+                       bit--;
+                    }
+                    else
+                    {
+                        if (len < text.Count)
+                        {
+                            //  gr = (text[len] == '1' ? true : false);
+                            //  Set(ref br, 0, gr);
+                            Console.WriteLine("Bit {0} {1}", len, text[len]);
+                            Set(ref br, 0, text[len++]);
+                            
+                        }
+                        if (len < text.Count)
+                        {
+                            //gr = (text[++len] == '1' ? true : false);
+                            //Set(ref bg, 0, gr);
+                            Console.WriteLine("Bit {0} {1}", len, text[len]);
+                            Set(ref bg, 0, text[len++]);
+                 
+                        }
+                        if (len < text.Count)
+                        {
+                            //gr = (text[++len] == '1' ? true : false);
+                            //Set(ref bb, 0, gr);
+                            Console.WriteLine("Bit {0} {1}", len, text[len]);
+                            Set(ref bb, 0, text[len++]);
+              
+                        }
+                        ncolor = Color.FromArgb(br, bg, bb);
+                        b.SetPixel(i, j, ncolor);
+                       
+                    }
+
+                }
+            pictureBox2.Image = b;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(textBox3.Text);
+            passwordBytes = SHA512.Create().ComputeHash(passwordBytes);
+            byte dlugosc_ciagu = 0;
+            bool[] bity = new bool[255 * 8];
+            Bitmap b = new Bitmap(pictureBox2.Image);
+            int x = b.Width;
+            int y = b.Height;
+            Color color;
+            int bit = 7;
+            int cbit = 0;
+            byte br, bg = 0, bb = 0;
+            bool gr;
+            int len = 0;
+            for (int i = 0; i < x; i++)
+                for (int j = 0; j < y; j++)
+                {
+                    color = b.GetPixel(i, j);
+                    br = color.R;
+                    bg = color.G;
+                    bb = color.B;
+                    if (bit >= 0)
+                    {
+
+                            //odczytanie długości ukrytego tekstu
+                            gr = Get((byte)br, 0);
+                            Set(ref dlugosc_ciagu, bit, gr);
+
+                            if (bit > 0)
+                            {
+                                bg = color.G;
+                                gr = Get((byte)bg, 0);
+                                Set(ref dlugosc_ciagu, --bit, gr);
+                            }
+                            if (bit > 0)
+                            {
+                                bb = color.B;
+                                gr = Get((byte)bb, 0);
+                                Set(ref dlugosc_ciagu, --bit, gr);
+                            }
+                            bit--;
+
+                        
+                    }
+                    else
+                    {
+                        if (dlugosc_ciagu * 8 > cbit)
+                        {
+                            gr = Get((byte)br, 0);
+                            bity[cbit] = gr;
+                            bg = color.G;
+                            gr = Get((byte)bg, 0);
+                            bity[++cbit] = gr;
+                            bb = color.B;
+                            gr = Get((byte)bb, 0);
+                            bity[++cbit] = gr;
+                            cbit++;
+                        }
+                    }
+                }
+            byte[] text = new byte[dlugosc_ciagu];
+            for (int i = 0; i<dlugosc_ciagu*8;i++)
+            {
+                Console.WriteLine("Bit {0} {1}", i, bity[i]);
+                if ((i != 0) && (i % 8) == 0) len++;
+                Set(ref text[len], (i%8), bity[i]);
+                
+            }
+            byte[] bytesDecrypted = AES.AES_Decrypt(text, passwordBytes);
+            String decoded;
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            decoded = ascii.GetString(bytesDecrypted);
+            //kod = decoded.Remove(dlugosc_ciagu);
+            textBox2.Text = decoded;
+        }
+    }
+}
